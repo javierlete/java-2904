36a032411c26a4171cff825b3acd137f3c9a0264
JavierLete <JavierLete@PC-JAVIER>
2025-12-30 15:57:51 +0100

    Separación de la funcionalidad de ejecutarSql en bibliotecas

---
+++ bases/src/bibliotecas/jdbc/ConectorSql.java
@@ -0,0 +1,59 @@
+package bibliotecas.jdbc;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.function.Function;
+
+import bibliotecas.fabrica.Fabrica;
+import daos.DaoException;
+
+public class ConectorSql {
+	private static final String URL;
+	private static final String USER;
+	private static final String PASS;
+
+	static {
+		try {
+			Properties props = new Properties();
+			props.load(Fabrica.class.getClassLoader().getResourceAsStream("fabrica.properties"));
+
+			URL = props.getProperty("URL");
+			USER = props.getProperty("USER");
+			PASS = props.getProperty("PASS");
+
+		} catch (Exception e) {
+			throw new DaoException("No se ha podido cargar el fabrica.properties", e);
+		}
+	}
+	
+	public static <T> Collection<T> ejecutarSql(String sql, Object... valores) {
+		return ejecutarSql(sql, null, valores);
+	}
+
+	public static <T> Collection<T> ejecutarSql(String sql, Function<ResultSet, T> mapeador, Object... valores) {
+		var objetos = new ArrayList<T>();
+
+		try (var con = DriverManager.getConnection(URL, USER, PASS); var pst = con.prepareStatement(sql);) {
+			for (int i = 0; i < valores.length; i++) {
+				pst.setObject(i + 1, valores[i]);
+			}
+
+			if (pst.execute()) {
+				var rs = pst.getResultSet();
+				while (rs.next()) {
+					objetos.add(mapeador.apply(rs));
+				}
+
+				return objetos;
+			} else {
+				return null;
+			}
+		} catch (SQLException e) {
+			throw new DaoException("Error en la consulta", e);
+		}
+	}
+}
--- bases/src/daos/DaoPersonaSqlite.java
+++ bases/src/daos/DaoPersonaSqlite.java
@@ -1,37 +1,17 @@
 package daos;
 
-import java.sql.DriverManager;
+import static bibliotecas.jdbc.ConectorSql.ejecutarSql;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.function.Function;
 
-import fabrica.Fabrica;
 import oop.Persona;
 import oop.PersonaDto;
 
 public class DaoPersonaSqlite implements DaoPersona {
-
-	private static final String URL;
-	private static final String USER;
-	private static final String PASS;
-
-	static {
-		try {
-			Properties props = new Properties();
-			props.load(Fabrica.class.getClassLoader().getResourceAsStream("fabrica.properties"));
-
-			URL = props.getProperty("URL");
-			USER = props.getProperty("USER");
-			PASS = props.getProperty("PASS");
-
-		} catch (Exception e) {
-			throw new DaoException("No se ha podido cargar el fabrica.properties", e);
-		}
-	}
 
 	@Override
 	public Collection<Persona> obtenerTodos() {
@@ -89,32 +69,5 @@ public class DaoPersonaSqlite implements DaoPersona {
 			throw new DaoException("Error en el mapeado de persona", e);
 		}
 	};
-
-	private <T> Collection<T> ejecutarSql(String sql, Object... valores) {
-		return ejecutarSql(sql, null, valores);
-	}
-
-	private <T> Collection<T> ejecutarSql(String sql, Function<ResultSet, T> mapeador, Object... valores) {
-		var objetos = new ArrayList<T>();
-
-		try (var con = DriverManager.getConnection(URL, USER, PASS); var pst = con.prepareStatement(sql);) {
-			for (int i = 0; i < valores.length; i++) {
-				pst.setObject(i + 1, valores[i]);
-			}
-
-			if (pst.execute()) {
-				var rs = pst.getResultSet();
-				while (rs.next()) {
-					objetos.add(mapeador.apply(rs));
-				}
-
-				return objetos;
-			} else {
-				return null;
-			}
-		} catch (SQLException e) {
-			throw new DaoException("Error en la consulta", e);
-		}
-	}
 
 }
