be13d46e4b883f585a13ff784fc66f8d967b7bbc
JavierLete <JavierLete@PC-JAVIER>
2026-01-12 16:23:13 +0100

    Cliente REST borrado

--- web/src/main/webapp/js/index.js
+++ web/src/main/webapp/js/index.js
@@ -3,20 +3,38 @@
 const URL = 'http://localhost:8080/web/api/v1/personas/';
 
 window.addEventListener('DOMContentLoaded', async () => {
-	const respuesta = await fetch(URL);
-	const personas = await respuesta.json();
-	
-	console.log(personas);
-	
-	const ul = document.querySelector('ul');
-	
-	for(const persona of personas) {
-		console.log(persona);
-		
-		const li = document.createElement('li');
-		
-		li.innerHTML = `${persona.id}: ${persona.nombre}, ${persona.fechaNacimiento}`;
-		
-		ul.appendChild(li);
-	}
+	await refrescarLista();
 });
+
+async function refrescarLista() {
+    const respuesta = await fetch(URL);
+    const personas = await respuesta.json();
+
+    console.log(personas);
+
+    const ul = document.querySelector('ul');
+
+	ul.innerHTML = '';
+	
+    for (const persona of personas) {
+        console.log(persona);
+
+        const li = document.createElement('li');
+
+        li.innerHTML = `${persona.id}: ${persona.nombre}, ${persona.fechaNacimiento}
+			<a href="javascript:borrar(${persona.id})">[Borrar]</a>
+		`;
+
+        ul.appendChild(li);
+    }
+}
+
+async function borrar(id) {
+	console.log('Borrar', id);
+	
+	const respuesta = await fetch(URL + id, { method: 'DELETE' });
+	
+	console.log(respuesta);
+	
+	respuesta.ok && await refrescarLista();
+}
