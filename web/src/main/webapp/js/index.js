87ed59744aaad1ebc00662f22eefce868d3b14f2
JavierLete <JavierLete@PC-JAVIER>
2026-01-12 16:34:12 +0100

    Cliente REST añadir

--- web/src/main/webapp/js/index.js
+++ web/src/main/webapp/js/index.js
@@ -2,8 +2,28 @@
 
 const URL = 'http://localhost:8080/web/api/v1/personas/';
 
+let form;
+
 window.addEventListener('DOMContentLoaded', async () => {
-	await refrescarLista();
+    await refrescarLista();
+
+    form = document.querySelector('form');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const persona = { nombre: form.nombre.value, fechaNacimiento: form.fecha.value };
+
+        const respuesta = await fetch(URL, {
+			method: 'POST',
+			body: JSON.stringify(persona),
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
+		
+		respuesta.ok && await refrescarLista();
+    });
 });
 
 async function refrescarLista() {
@@ -14,8 +34,8 @@ async function refrescarLista() {
 
     const ul = document.querySelector('ul');
 
-	ul.innerHTML = '';
-	
+    ul.innerHTML = '';
+
     for (const persona of personas) {
         console.log(persona);
 
@@ -30,11 +50,11 @@ async function refrescarLista() {
 }
 
 async function borrar(id) {
-	console.log('Borrar', id);
-	
-	const respuesta = await fetch(URL + id, { method: 'DELETE' });
-	
-	console.log(respuesta);
-	
-	respuesta.ok && await refrescarLista();
+    console.log('Borrar', id);
+
+    const respuesta = await fetch(URL + id, { method: 'DELETE' });
+
+    console.log(respuesta);
+
+    respuesta.ok && await refrescarLista();
 }
