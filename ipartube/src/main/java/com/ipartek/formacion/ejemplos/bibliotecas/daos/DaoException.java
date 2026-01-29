0ced906bac76aea3b28ba6bff2fea43aee20a240
JavierLete <JavierLete@PC-JAVIER>
2026-01-29 17:40:06 +0100

    DaoException "Lombokizado"

--- ipartube/src/main/java/com/ipartek/formacion/ejemplos/bibliotecas/daos/DaoException.java
+++ ipartube/src/main/java/com/ipartek/formacion/ejemplos/bibliotecas/daos/DaoException.java
@@ -1,27 +1,8 @@
 package com.ipartek.formacion.ejemplos.bibliotecas.daos;
 
+import lombok.experimental.StandardException;
+
+@StandardException
 public class DaoException extends RuntimeException {
-
 	private static final long serialVersionUID = -3635303431739865823L;
-
-	public DaoException() {
-		super();
-	}
-
-	public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public DaoException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public DaoException(String message) {
-		super(message);
-	}
-
-	public DaoException(Throwable cause) {
-		super(cause);
-	}
-
 }
