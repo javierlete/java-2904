8ca4c8c929bfa9efc47485c7fca10837526e6c39
JavierLete <JavierLete@PC-JAVIER>
2026-02-02 16:16:15 +0100

    ipartubespring: Spring Starter Project/Lombok, JPA, Validation

---
+++ ipartubespring/src/main/java/com/ipartek/formacion/ejemplos/ipartube/IpartubespringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.ipartube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartubespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartubespringApplication.class, args);
+	}
+
+}
---
+++ ipartubespring/src/test/java/com/ipartek/formacion/ejemplos/ipartube/IpartubespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.ipartube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartubespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
