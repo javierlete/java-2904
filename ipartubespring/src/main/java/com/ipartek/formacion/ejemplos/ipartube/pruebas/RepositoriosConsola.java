63b35784f49a765b04b50ffd3b2e79cf30d1a4e3
JavierLete <JavierLete@PC-JAVIER>
2026-02-03 17:23:59 +0100

    Datos de prueba de video

--- ipartubespring/src/main/java/com/ipartek/formacion/ejemplos/ipartube/pruebas/RepositoriosConsola.java
+++ ipartubespring/src/main/java/com/ipartek/formacion/ejemplos/ipartube/pruebas/RepositoriosConsola.java
@@ -7,28 +7,40 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.ipartek.formacion.ejemplos.ipartube.entidades.Usuario;
+import com.ipartek.formacion.ejemplos.ipartube.entidades.Video;
 import com.ipartek.formacion.ejemplos.ipartube.repositorios.UsuarioRepository;
+import com.ipartek.formacion.ejemplos.ipartube.repositorios.VideoRepository;
 
 @Component
 public class RepositoriosConsola implements CommandLineRunner {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private VideoRepository videoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		usuarioRepository.save(Usuario.builder().nombre("Javier").email("javier@email.net").password("javier").build());
+		var javier = Usuario.builder().nombre("Javier").email("javier@email.net").password("javier").build();
 
-		usuarioRepository
-				.saveAll(List.of(Usuario.builder().nombre("Pepe").email("pepe@email.net").password("pepe").build(),
-						Usuario.builder().nombre("Juan").email("juan@email.net").password("juan").build(),
-						Usuario.builder().nombre("Pedro").email("pedro@email.net").password("pedro").build()));
+		usuarioRepository.save(javier);
+
+		var pepe = Usuario.builder().nombre("Pepe").email("pepe@email.net").password("pepe").build();
+		var juan = Usuario.builder().nombre("Juan").email("juan@email.net").password("juan").build();
+		var pedro = Usuario.builder().nombre("Pedro").email("pedro@email.net").password("pedro").build();
 		
+		usuarioRepository.saveAll(List.of(pepe, juan, pedro));
+
 		for (var usuario : usuarioRepository.findAll()) {
 			System.out.println(usuario);
 		}
-		
+
 		System.out.println(usuarioRepository.findByEmail("juan@email.net"));
+
+		var video1 = Video.builder().propietario(pedro).titulo("Prueba 1").videoUrl("http://video").build();
+		
+		videoRepository.save(video1);
 	}
 
 }
