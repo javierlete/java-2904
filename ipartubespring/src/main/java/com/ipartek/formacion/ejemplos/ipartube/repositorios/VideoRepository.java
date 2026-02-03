c4381662be4629d56a752d19154ee188d7b6fa15
JavierLete <JavierLete@PC-JAVIER>
2026-02-03 17:25:29 +0100

    Nombre explícito ruta Data REST y método con JPQL propio con DTOs

--- ipartubespring/src/main/java/com/ipartek/formacion/ejemplos/ipartube/repositorios/VideoRepository.java
+++ ipartubespring/src/main/java/com/ipartek/formacion/ejemplos/ipartube/repositorios/VideoRepository.java
@@ -2,10 +2,22 @@ package com.ipartek.formacion.ejemplos.ipartube.repositorios;
 
 import java.util.Collection;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.ejemplos.ipartube.entidades.Video;
 
+@RepositoryRestResource(collectionResourceRel = "videos", path = "videos")
 public interface VideoRepository extends CrudRepository<Video, Long> {
 	Collection<Video> findByPropietarioId(Long idPropietario);
+
+	@Query("""
+			SELECT new com.ipartek.formacion.ejemplos.ipartube.dtos.VideoDto(
+				v.id, v.titulo, v.descripcion, v.fechaHora, v.imagenUrl, v.videoUrl, 
+					new com.ipartek.formacion.ejemplos.ipartube.dtos.UsuarioDto(p.id, p.nombre)
+			) 
+			FROM Video v LEFT JOIN v.propietario p
+			""")
+	<T> Collection<T> findBy(Class<T> tipo);
 }
