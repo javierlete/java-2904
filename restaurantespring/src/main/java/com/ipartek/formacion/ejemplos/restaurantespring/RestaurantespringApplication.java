6a3e7ac4bd2495ad02201b466fa8fd17b20a8702
JavierLete <JavierLete@PC-JAVIER>
2026-02-11 15:18:52 +0100

    RestauranteSpring: Lombok, JPA, Validation

---
+++ restaurantespring/src/main/java/com/ipartek/formacion/ejemplos/restaurantespring/RestaurantespringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.restaurantespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantespringApplication.class, args);
+	}
+
+}
---
+++ restaurantespring/src/test/java/com/ipartek/formacion/ejemplos/restaurantespring/RestaurantespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.restaurantespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
