ad26b0ccb55e1b3a8e4fc3896ba288b6b90c1b71
JavierLete <JavierLete@PC-JAVIER>
2026-02-18 15:49:02 +0100

    Cambio a seguridad por base de datos a través de JDBC

--- restaurantespring/src/main/java/com/ipartek/formacion/ejemplos/restaurantespring/config/WebSecurityConfig.java
+++ restaurantespring/src/main/java/com/ipartek/formacion/ejemplos/restaurantespring/config/WebSecurityConfig.java
@@ -1,16 +1,17 @@
 package com.ipartek.formacion.ejemplos.restaurantespring.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,27 +20,13 @@ class WebSecurityConfig {
 
 	// Autenticación
 	@Bean
-	UserDetailsService userDetailsService(PasswordEncoder encoder) {
-		// @formatter:off
-		UserDetails javier = User
-				.withUsername("javier@email.net")
-				.password(encoder.encode("Javier1!"))
-				.roles("ADMINISTRADOR")
-			.build();
-			
-		UserDetails pepe = User
-				.withUsername("pepe@email.net")
-				.password(encoder.encode("Pepe1!"))
-				.roles("USUARIO")
-			.build();
-		// @formatter:on
-
-		return new InMemoryUserDetailsManager(javier, pepe);
+	UserDetailsService userDetailsService(PasswordEncoder encoder, DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance(); // new BCryptPasswordEncoder();
 	}
 
 	// Autorización
